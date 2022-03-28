9205d17101d48c584e689a252b90fb680dfcea56
Roman <romanian.ua@gmail.com>
2022-03-28 14:59:37 +0300

    Add models

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test',
+        'NAME': 'shop',
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
@@ -122,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- store/models.py
+++ store/models.py
@@ -1,6 +1,72 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
-# Create your models here.
-class Bike(models.Model):
-    pass
+class Brand(models.Model):
+    name = models.CharField('Brand name', max_length=25)
+    country = models.CharField('Made in country', max_length=25)
+    note = models.TextField('Notes', blank=True, null=True, max_length=400)
+
+    class Meta:
+        verbose_name = 'Brand bike'
+        verbose_name_plural = 'Brands bike'
+
+    def __str__(self):
+        return self.name
+
+
+class BikeModel(models.Model):
+    CATEGORY_BIKES = [
+        ('MTB', 'Mountain'),
+        ('EBike', 'Electric Bike'),
+        ('Cross', 'Cross Bike'),
+        ('Child', 'Child Bike'),
+        ('Road', 'Road Bike'),
+    ]
+    name = models.CharField('Model name', max_length=40)
+    brand = models.ForeignKey('Brand', on_delete=models.PROTECT)
+    type = models.CharField('Types of bike', max_length=15, choices=CATEGORY_BIKES)
+
+    class Meta:
+        verbose_name = 'Model bike'
+        verbose_name_plural = 'Models bike'
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    SIZE_FRAME = [
+        ('XS', 'Extra Small'),
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+        ('XL', 'Extra Large'),
+        ('XXL', 'Extra Extra Large'),
+    ]
+    title = models.CharField('Title post', max_length=75)
+    description = models.TextField('Description')
+    model = models.ForeignKey('BikeModel', on_delete=models.PROTECT)
+    size = models.CharField('Size frame', max_length=15, choices=SIZE_FRAME)
+    pub_date = models.DateTimeField('Public date', default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Bike'
+        verbose_name_plural = 'Bikes'
+
+    def __str__(self):
+        return self.title
+
+
+class ImageBike(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    image = models.ImageField('Photo bike', upload_to='bike/%Y/%m/%d/')
+
+    class Meta:
+        verbose_name = 'Photo bike'
+        verbose_name_plural = 'Photo bikes'
+
+    def __str__(self):
+        return self.post
